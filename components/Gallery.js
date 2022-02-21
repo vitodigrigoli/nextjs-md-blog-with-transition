10e4ce21f4459a187581aac27b666240d9075c15
vitodigrigoli <73437231+vitodigrigoli@users.noreply.github.com>
2022-02-21 13:33:39 +0100

    gallery component

---
+++ components/Gallery.js
@@ -0,0 +1,14 @@
+import ImageGallery from 'react-image-gallery';
+import {useEffect} from "react";
+
+export default function Gallery({title, images}){
+	useEffect( () => console.log(images))
+	return(
+		<section className="gallery container">
+			<h3>{title}</h3>
+			<div className="gallery__images">
+				<ImageGallery items={images} thumbnailPosition={"bottom"} disableSwipe={false} />
+			</div>
+		</section>
+	)
+}
--- pages/sport.js
+++ pages/sport.js
@@ -1,7 +1,72 @@
+import Hero from "../components/Hero";
+import cover from "../public/referal.jpg";
+import Banner from "../components/Banner";
+import Focuses from "../components/Focuses";
+import Gallery from "../components/Gallery";
+
+
+const HeroText = {
+	title: 'I nostri <span class="orange">Sport</span> [25]',
+	text: "Vestibulum porttitor nisl eget nulla vulputate, a maximus dolor ultricies. Cras ut nulla nec turpis porttitor dictum. Ut id dictum erat. [250]"
+}
+
+const bannerText = {
+	title: "Lorem ipsum dolor sit amet [25]",
+	text: "Vestibulum porttitor nisl eget nulla vulputate, a maximus dolor ultricies. Cras ut nulla nec turpis porttitor dictum. Ut id dictum erat. [250]"
+}
+
+const focuses = {
+	title: 'Lorem ipsum dolor sit amet [25]',
+	list: [
+		{
+			title: "Laboratorio #1",
+			text: "Vestibulum porttitor nisl eget nulla vulputate, a maximus dolor ultricies. Cras ut nulla nec turpis porttitor dictum. Ut id dictum erat. [150]"
+		},
+		{
+			title: "Laboratorio #2",
+			text: "Vestibulum porttitor nisl eget nulla vulputate, a maximus dolor ultricies. Cras ut nulla nec turpis porttitor dictum. Ut id dictum erat. [150]"
+		},
+		{
+			title: "Laboratorio #3",
+			text: "Vestibulum porttitor nisl eget nulla vulputate, a maximus dolor ultricies. Cras ut nulla nec turpis porttitor dictum. Ut id dictum erat. [150]"
+		},
+		{
+			title: "Laboratorio #4",
+			text: "Vestibulum porttitor nisl eget nulla vulputate, a maximus dolor ultricies. Cras ut nulla nec turpis porttitor dictum. Ut id dictum erat. [150]"
+		},
+	]
+}
+const galleryText = {
+	title: 'Lorem ipsum dolor sit amet [25]',
+	images: [
+		{
+			original: cover.src,
+			thumbnail: cover.src,
+		},
+		{
+			original: cover.src,
+			thumbnail: cover.src,
+		},
+		{
+			original: cover.src,
+			thumbnail: cover.src,
+		},
+		{
+			original: cover.src,
+			thumbnail: cover.src,
+		},
+	]
+}
+
+
+
 export default function Sport(){
 	return(
-		<div className="page page-four">
-			<h1>Sport</h1>
-		</div>
+		<>
+			<Hero title={HeroText.title} text={HeroText.text} media={cover} />
+			<Banner title={bannerText.title} text={bannerText.text} withButton />
+			<Focuses title={focuses.title}  focuses={focuses.list}/>
+			<Gallery title={galleryText.title} images={galleryText.images} />
+		</>
 	)
 }
